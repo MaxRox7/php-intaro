86876fc1107e61d841e7adf0544add3c4e568407
Maxim Boldyrev <maxrox1904@gmail.com>
2024-05-23 17:49:51 +0300

    hanging around

---
+++ classes/person.class.php
@@ -0,0 +1,30 @@
+<?php 
+
+class Person{
+
+    public $name;
+
+
+    public function __construct($name) {
+        $this->name = $name;
+
+
+
+    }
+
+
+    public function setname($name) {
+        $this->name = $name;
+
+
+
+    }
+
+    public function getname() {
+        return $this->name;
+
+
+    }
+
+}
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php       
+include 'includes/loader.php';
+?>
+
+<?php      
+$obj = new Person("maxim");
+
+
+echo $obj->getname();
+echo $obj-> name;
+
+
+
+
+
